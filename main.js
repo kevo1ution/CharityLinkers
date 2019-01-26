15fad89af2033e7711889b538d64512085c173f1
root <root@ip-172-31-30-254.us-east-2.compute.internal>
2019-01-26 20:40:17 +0000

    setup nodemodules

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 var http = require('http');
 var url = require('url');
+var bparser = require('body-parser');
 
 http.createServer(function(req, res){
 	res.writeHead(200, {'Content-Type': 'text/html'});
 	var q = url.parse(req.url, true).query;
 	var txt = q.year + " " + q.month;
-	res.end(txt);
+	res.write(txt);
+	res.statusCode = 404;
+	res.end();
 }).listen(8080);
 
