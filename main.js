50678b953d1fd73f55f45ef64faa66f240d69074
Kevin Tang <kevin26428@gmail.com>
2019-01-26 14:54:16 -0600

    Switched to Express js

--- main.js
+++ main.js
@@ -1,7 +1,18 @@
 var http = require('http');
+var express = require('express');
 var url = require('url');
-var bparser = require('body-parser');
+var bodyparser = require('body-parser');
 
+var app = express();
+
+app.use(bodyParser.json());
+
+app.get('', (req,res) =>{
+	var q = url.parse(req.url, true).query;
+	res.json(q);
+}).listen(8080);
+
+/*
 http.createServer(function(req, res){
 	res.writeHead(200, {'Content-Type': 'text/html'});
 	var q = url.parse(req.url, true).query;
@@ -10,4 +21,4 @@ http.createServer(function(req, res){
 	res.statusCode = 404;
 	res.end();
 }).listen(8080);
-
+*/
