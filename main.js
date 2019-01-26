71e06302d97008eedbc9e0376dd8a57c9198fee1
Kevin Tang <kevin26428@gmail.com>
2019-01-26 14:21:13 -0600

    sending json information through http

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 var http = require('http');
+var url = require('url');
 
 http.createServer(function(req, res){
-	res.writeHead(200, {'Content-Type': 'text/plain'});
-	res.end('Hello world!');
+	res.writeHead(200, {'Content-Type': 'text/html'});
+	var q = url.parse(req.url, true).query;
+	var txt = q.year + " " + q.month;
+	res.end(txt);
 }).listen(8080);
 
