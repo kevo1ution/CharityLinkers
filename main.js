0b57d58f5c78ea1681f01d08d1ddb0a70d337e39
root <root@ip-172-31-30-254.us-east-2.compute.internal>
2019-01-27 02:11:12 +0000

    Hacked together photo recognition nodejs

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ var fs = require('fs');
 
 
 var subscriptionKey = '506c4842d0de4dbb924262eed9728352';
-var uriBase = 'https://westcentralus.api.cognitive.microsoft.com/vision/v2.0';
+var uriBase = 'https://westcentralus.api.cognitive.microsoft.com/vision/v2.0/analyze';
 
 var app = express();
 app.use(bodyparser.json({limit: '50mb'}));
@@ -23,9 +23,32 @@ app.get('/search', (req,res) =>{
 app.post('/image', (req, res) =>{
 	var buff = new Buffer(req.body.image, 'base64');
 	fs.writeFileSync('./pics/imageSearch.png', buff);
-	var data = fs.readFileSync('./pics/imageSearch.png');
-	console.log(data);
+	var dat = fs.readFileSync('./pics/imageSearch.png');
+	//console.log(data.toString());
 
+	const params = {
+		'visualFeatures': 'Categories,Description,Tags,Color',
+		'details': '',
+		'language': 'en'
+	};
+	const options = {
+		uri: uriBase,
+		qs: params,
+		body: dat,
+		headers: {
+			'ocp-apim-subscription-key' : '506c4842d0de4dbb924262eed9728352',
+			'Content-Type': 'application/octet-stream'
+		}
+	}
+
+	request.post(options, (err, response, body) => {
+		if (err) {
+			console.log('Error: ', err);
+			return;
+		}
+		jsonResponse = JSON.stringify(JSON.parse(body), null, '	');
+		console.log(jsonResponse);	
+	});
 	//res.json({msg: "nice69696969"})
 });
 
